03d8e35ba4de3b664a091d20bb50b39400296e77
yojanzambrano <114314348+yojanzambrano@users.noreply.github.com>
2026-05-19 06:10:03 -0500

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Estos datos los sacas de tu panel de Supabase:
-// Settings -> API -> Project URL y Project API keys (anon public)
-const supabaseUrl = 'https://lpohmrnblmlbkdwyumvt.supabase.co';
-const supabaseAnonKey = 'sb_publishable_r9J6aU44GMOPon11xlMbyg_M2gdU1DP';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
